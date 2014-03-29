5a2ead9976d60fdccb7b7403b47ebf4193ebe781
Paul Bellamy <paul.a.bellamy@gmail.com>
2014-03-29 16:38:27 -0700

    updating docs

--- src/infinite_multi_reader.go
+++ src/infinite_multi_reader.go
@@ -5,11 +5,9 @@ import (
 	"io"
 )
 
-var EmptyReader = &bytes.Buffer{}
-
 type infiniteMultiReader struct {
 	currentReader io.Reader
-	callback      func() (io.Reader, error)
+	nextReader    func() (io.Reader, error)
 }
 
 func (imr *infiniteMultiReader) Read(p []byte) (n int, err error) {
@@ -23,19 +21,21 @@ func (imr *infiniteMultiReader) Read(p []byte) (n int, err error) {
 			}
 			return
 		}
-		if imr.currentReader, err = imr.callback(); err != nil {
+		if imr.currentReader, err = imr.nextReader(); err != nil {
 			return
 		}
 	}
 	return 0, io.EOF
 }
 
-// MultiReader returns a Reader that's the logical concatenation of
-// the provided input readers.  They're read sequentially.  Once all
-// inputs are drained, Read will return EOF.
-func InfiniteMultiReader(callback func() (io.Reader, error)) io.Reader {
+// InfiniteMultiReader returns a Reader that's the logical concatenation of the
+// input readers returned by the nextReader function. They're read
+// sequentially. When there are no more readers, the nextReader function should
+// return the io.EOF error. Once all inputs are drained, Read will return
+// io.EOF.
+func InfiniteMultiReader(nextReader func() (io.Reader, error)) io.Reader {
 	return &infiniteMultiReader{
-		currentReader: EmptyReader,
-		callback:      callback,
+		currentReader: &bytes.Buffer{},
+		nextReader:    nextReader,
 	}
 }
