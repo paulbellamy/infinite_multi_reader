feae1fd5844a5561c9e2298da30a23eb30208881
Paul Bellamy <paul.a.bellamy@gmail.com>
2014-03-30 21:30:17 -0700

    running go fmt on test file

--- src/infinite_multi_reader_test.go
+++ src/infinite_multi_reader_test.go
@@ -2,31 +2,31 @@ package infinite_multi_reader
 
 import (
 	"bytes"
-  "io"
-  "io/ioutil"
-  "testing"
+	"io"
+	"io/ioutil"
+	"testing"
 )
 
 func TestInfiniteMultiReader(t *testing.T) {
-  readers := make(chan io.Reader)
-  go func() {
-    readers <- bytes.NewBufferString("hello")
-    readers <- bytes.NewBufferString(" ")
-    readers <- bytes.NewBufferString("world")
-    close(readers)
-  }()
-  r := InfiniteMultiReader(func() (reader io.Reader, err error) {
-    reader, ok := <-readers
-    if !ok {
-      err = io.EOF
-    }
-    return
-  })
-  result, err := ioutil.ReadAll(r)
-  if err != nil {
-    t.Fatal(err)
-  }
-  if string(result) != "hello world" {
-    t.Fatal("Unexpected result:", string(result))
-  }
+	readers := make(chan io.Reader)
+	go func() {
+		readers <- bytes.NewBufferString("hello")
+		readers <- bytes.NewBufferString(" ")
+		readers <- bytes.NewBufferString("world")
+		close(readers)
+	}()
+	r := InfiniteMultiReader(func() (reader io.Reader, err error) {
+		reader, ok := <-readers
+		if !ok {
+			err = io.EOF
+		}
+		return
+	})
+	result, err := ioutil.ReadAll(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(result) != "hello world" {
+		t.Fatal("Unexpected result:", string(result))
+	}
 }
